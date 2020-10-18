db77df918ef4896a3a3df527f845b0b505a261b2
fuermosi777 <liuhao1990@gmail.com>
2020-10-18 16:35:13 -0700

    try add link

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,11 @@ import { phraseEmphasis } from './phraseEmphasis';
 import './styles.less';
 import { heading } from './heading';
 import { wordmarkTheme } from './wordmarkTheme';
+import { link } from './link';
 
 let startState = EditorState.create({
   doc: '',
-  extensions: [wordmarkTheme(), phraseEmphasis(), heading(), keymap(defaultKeymap)],
+  extensions: [wordmarkTheme(), phraseEmphasis(), heading(), link(), keymap(defaultKeymap)],
 });
 
 let view = new EditorView({
---
+++ src/link.ts
@@ -0,0 +1,110 @@
+import { Extension } from '@codemirror/next/state';
+import {
+  Decoration,
+  DecorationSet,
+  EditorView,
+  Range,
+  ViewPlugin,
+  ViewUpdate,
+  WidgetType,
+} from '@codemirror/next/view';
+import { isCursorInside } from './utils';
+
+const linkRE = /\[([^\[\]]+)\]\(([^\)\(\s]+)(?:\s"([^\"]+)")?\)/g;
+
+export function link(): Extension {
+  return [linkDecorationPlugin];
+}
+
+const linkDecorationPlugin = ViewPlugin.fromClass(
+  class {
+    decorations: DecorationSet = Decoration.none;
+
+    constructor(public view: EditorView) {
+      this.recompute();
+    }
+
+    recompute(update?: ViewUpdate) {
+      let decorations: Range<Decoration>[] = [];
+      for (let { from, to } of this.view.visibleRanges) {
+        this.getDecorationsFor(from, to, decorations);
+      }
+      decorations.sort((deco1, deco2) => (deco1.from > deco2.from ? 1 : -1));
+      this.decorations = Decoration.set(decorations);
+
+      this.decorations = this.decorations.update({
+        filter: (from, to, value: Decoration) => {
+          if (update && isCursorInside(update, from, to)) {
+            return false;
+          }
+
+          return true;
+        },
+      });
+    }
+
+    update(update: ViewUpdate) {
+      if (update.changes.length || update.viewportChanged) {
+        this.recompute(update);
+      }
+    }
+
+    getDecorationsFor(from: number, to: number, decorations: Range<Decoration>[]) {
+      let { doc } = this.view.state;
+
+      for (let pos = from, cursor = doc.iterRange(from, to), m; !cursor.next().done; ) {
+        if (!cursor.lineBreak) {
+          while ((m = linkRE.exec(cursor.value))) {
+            const linkDecoration = Decoration.replace({
+              widget: new LinkWidget({
+                displayText: m[1],
+                url: m[2],
+                title: m[3],
+              }),
+              inclusive: true,
+            });
+            decorations.push(linkDecoration.range(pos + m.index, pos + m.index + m[0].length));
+          }
+        }
+        pos += cursor.value.length;
+      }
+    }
+  },
+  {
+    decorations: (v) => v.decorations,
+  },
+);
+
+interface LinkWidgetSpec {
+  readonly displayText: string;
+  readonly url?: string;
+  readonly title?: string;
+  // [an example][id] reference style link
+  readonly ref?: string;
+}
+
+class LinkWidget extends WidgetType {
+  constructor(readonly spec: LinkWidgetSpec) {
+    super();
+  }
+
+  eq(other: LinkWidget) {
+    return this.spec.displayText === other.spec.displayText && this.spec.url === other.spec.url;
+  }
+
+  toDOM() {
+    let link = document.createElement('a');
+    link.textContent = this.spec.displayText;
+    if (this.spec.url) {
+      link.href = this.spec.url;
+    }
+    if (this.spec.title) {
+      link.title = this.spec.title;
+    }
+    return link;
+  }
+
+  ignoreEvent(): boolean {
+    return false;
+  }
+}
